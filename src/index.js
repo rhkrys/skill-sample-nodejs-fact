1f72611245ff902b25f5eeb6f061a73d875b7a49
rhkrys <crystal.cp@gmail.com>
2017-02-07 14:08:52 -0500

    Updated App ID and Typos

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 'use strict';
 var Alexa = require('alexa-sdk');
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+var APP_ID = ' amzn1.ask.skill.5cc61b6d-8a2f-440a-9877-ec4193002cc8';  // TODO replace with your app ID (OPTIONAL)
 
 var languageStrings = {
     "en-GB": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+                'In case you ever foolishly forget I am never not thinking about you.',
+                'You know you are in love when you can not fall asleep because reality is finally better than your dreams.',
+                'There is never a time or place for true love. It happens accidentally in a heartbeat in a single flashing throbbing moment.',
+                'For the two of us home is not a place it is a person and we are finally home.',
+                'They say when you are missing someone that they are probably feeling the same but I dont think its possible for you to miss me as much as Im missing you right now.',
+                'So it is not gonna be easy. It is going to be really hard we are gonna have to work at this everyday but I want to do that because I want you. I want all of you forever everyday. You and me ... every day.',
+                'Anyone who falls in love is searching for the missing pieces of themselves. So anyone whos in love gets sad when they think of their lover. Its like stepping back inside a room you have fond memories of one you havent seen in a long time.',
+                'Love is like the wind you cant see it but you can feel it.',
+                'To love at all is to be vulnerable.',
+                'Every heart sings a song incomplete until another heart whispers back. Those who wish to sing always find a song. At the touch of a lover everyone becomes a poet.',
+                'We love with a love that was more than love.',
+                'Love looks not with the eyes but with the heart.',
             ],
-            "SKILL_NAME" : "British Space Facts",
-            "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+            "SKILL_NAME" : "British Heart Facts",
+            "GET_FACT_MESSAGE" : "Here's your heart fact: ",
+            "HELP_MESSAGE" : "You can say tell me a heart fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
         }
@@ -30,51 +29,26 @@ var languageStrings = {
     "en-US": {
         "translation": {
             "FACTS": [
-                'In case you ever foolishly forget, I am never not thinking about you.',
-                'You know you're in love when you can't fall asleep because reality is finally better than your dreams.',
-                'There is never a time or place for true love. It happens accidentally, in a heartbeat, in a single flashing, throbbing moment.',
-                'I love you without knowing how, or when, or from where. I love you simply, without problems or pride: I love you in this way because I do not know any other way of loving but this, in which there is no I or you, so intimate that your hand upon my chest is my hand, so intimate that when I fall asleep your eyes close.',
-                'For the two of us, home isn't a place. It is a person. And we are finally home.',
-                'They say when you are missing someone that they are probably feeling the same, but I don't think it's possible for you to miss me as much as I'm missing you right now.',
-                'So it's not gonna be easy. It's going to be really hard; we're gonna have to work at this everyday, but I want to do that because I want you. I want all of you, forever, everyday. You and me ... every day.',
-                'Anyone who falls in love is searching for the missing pieces of themselves. So anyone who's in love gets sad when they think of their lover. It's like stepping back inside a room you have fond memories of, one you haven't seen in a long time.',
-                'Love is like the wind, you can't see it but you can feel it.',
+                'In case you ever foolishly forget I am never not thinking about you.',
+                'You know you are in love when you can not fall asleep because reality is finally better than your dreams.',
+                'There is never a time or place for true love. It happens accidentally in a heartbeat in a single flashing throbbing moment.',
+                'For the two of us home is not a place it is a person and we are finally home.',
+                'They say when you are missing someone that they are probably feeling the same but I dont think its possible for you to miss me as much as Im missing you right now.',
+                'So it is not gonna be easy. It is going to be really hard we are gonna have to work at this everyday but I want to do that because I want you. I want all of you forever everyday. You and me ... every day.',
+                'Anyone who falls in love is searching for the missing pieces of themselves. So anyone whos in love gets sad when they think of their lover. Its like stepping back inside a room you have fond memories of one you havent seen in a long time.',
+                'Love is like the wind you cant see it but you can feel it.',
                 'To love at all is to be vulnerable.',
-                'Every heart sings a song, incomplete, until another heart whispers back. Those who wish to sing always find a song. At the touch of a lover, everyone becomes a poet.',
+                'Every heart sings a song incomplete until another heart whispers back. Those who wish to sing always find a song. At the touch of a lover everyone becomes a poet.',
                 'We love with a love that was more than love.',
-                'Love looks not with the eyes, but with the heart.',
+                'Love looks not with the eyes but with the heart.',
             ],
-            "SKILL_NAME" : "American Space Facts",
-            "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+            "SKILL_NAME" : "American Heart Facts",
+            "GET_FACT_MESSAGE" : "Here's your heart fact: ",
+            "HELP_MESSAGE" : "You can say tell me a heart fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
         }
-    },
-    "de-DE": {
-        "translation": {
-            "FACTS": [
-                "Ein Jahr dauert auf dem Merkur nur 88 Tage.",
-                "Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.",
-                "Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.",
-                "Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.",
-                "Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.",
-                "Jupiter hat den kürzesten Tag aller Planeten.",
-                "Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.",
-                "Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.",
-                "Die Sonne ist eine fast perfekte Kugel.",
-                "Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.",
-                "Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.",
-                "Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.",
-                "Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr."
-            ],
-            "SKILL_NAME" : "Weltraumwissen auf Deutsch",
-            "GET_FACT_MESSAGE" : "Hier sind deine Fakten: ",
-            "HELP_MESSAGE" : "Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?",
-            "HELP_REPROMPT" : "Wie kann ich dir helfen?",
-            "STOP_MESSAGE" : "Auf Wiedersehen!"
-        }
-    }
+}
 };
 
 exports.handler = function(event, context, callback) {
